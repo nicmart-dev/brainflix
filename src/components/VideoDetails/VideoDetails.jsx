97501efd140b38a189d076cd6e420fd977146be7
Nicolas Martinez <nicmart@gmail.com>
2024-05-02 19:20:05 -0700

    complete styling for mobile

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -11,17 +11,25 @@ function VideoDetails() {
       </h1>
       <div className="video-details__metadata-container">
         <h2 className="video-details__channel">By Aiden Thompson</h2>
-        <div>
+        <div className="video-details__stats-container">
           <img src={viewsIcon} alt="views icon" />
           <span className="video-details__views">980,544</span>
         </div>
         <span className="video-details__date">8/8/2023</span>
 
-        <div>
+        <div className="video-details__stats-container">
           <img src={likesIcon} alt="likes icon" />
           <span className="video-details__likes">22,479</span>
         </div>
       </div>
+      <p className="video-details__description">
+        Explore the cutting-edge developments and predictions for Artificial
+        Intelligence in the coming years. From revolutionary breakthroughs in
+        machine learning to the ethical considerations influencing AI
+        advancements, this exploration transcends the boundaries of mere
+        speculation. Join us on a journey that navigates the intricate interplay
+        between innovation, ethics, and the ever-evolving tech frontier.
+      </p>
     </article>
   );
 }
