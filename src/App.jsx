0c31fb8f21a1365b62799a1c5a93ea102cfdb60a
Nicolas Martinez <nicmart@gmail.com>
2024-05-04 10:03:12 -0700

    set state boilerplate importing video json

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.scss";
 import Comments from "./components/Comments/Comments";
 import NextVideos from "./components/NextVideos/NextVideos";
@@ -5,7 +6,24 @@ import SiteHeader from "./components/SiteHeader/SiteHeader";
 import VideoDetails from "./components/VideoDetails/VideoDetails";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 
+import videoData from "./data/video-details.json";
+
 function App() {
+  const [videos, setVideo] = useState(videoData);
+  const [selectedVideo, setSelectedVideo] = useState({});
+
+  const handleVideoClick = (id) => {
+    console.log(id);
+    console.log("video clicked");
+
+    //look for matching video
+    const foundVideo = videos.find((video) => video.id === id);
+    console.log(foundVideo);
+
+    // set found video to currently selected in state
+    setSelectedVideo(foundVideo);
+  };
+
   return (
     <div className="App">
       <SiteHeader />
@@ -13,11 +31,18 @@ function App() {
         <VideoPlayer />
         <section className="App__post-video-container">
           <div className="App__video-comments-container">
-            <VideoDetails />
+            <VideoDetails
+              videoList={videos}
+              selectedVideoId={selectedVideo.id}
+            />
             <Comments />
           </div>
           <aside className="App__next-videos-container">
-            <NextVideos />
+            <NextVideos
+              videoList={videos}
+              selectedVideoId={selectedVideo.id}
+              handleVideoClick={handleVideoClick}
+            />
           </aside>
         </section>
       </main>
