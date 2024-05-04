1bc8de859b29c22b10abf438d52648fca705081c
Nicolas Martinez <nicmart@gmail.com>
2024-05-04 12:16:46 -0700

    remove unused old html

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -26,22 +26,6 @@ function NextVideos({ videoList, selectedVideoId, handleVideoClick }) {
               </div>
             </li>
           ))}
-
-        {/* <li className="next-videos__video-list-item">
-          <a href="#" className="next-videos__video-link">
-            <img
-              className="next-videos__video-thumbnail"
-              src="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image2.jpg"
-              alt="video thumbnail"
-            />
-          </a>
-          <div className="next-videos__video-info">
-            <span className="next-videos__video-title">
-              Molecular Gastronomy: Secrets Unveiled
-            </span>
-            <span>Cornelia Currey</span>
-          </div>
-        </li> */}
       </ul>
     </section>
   );
