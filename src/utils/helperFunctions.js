0213c69ca706cf167329cf5f98df978153baa45a
Nicolas Martinez <nicmart@gmail.com>
2024-05-05 14:19:21 -0700

    correct date format

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -4,7 +4,7 @@
 
 export function formatDate(timestamp) {
     const date = new Date(timestamp);
-    return date.toLocaleDateString('en-us', { month: '2-digit', day: '2-digit', year: 'numeric', timeZone: 'UTC' }).split(",").join("");
+    return date.toLocaleDateString('en-us', { month: 'numeric', day: '2-digit', year: 'numeric', timeZone: 'UTC' }).split(",").join("");
 }
 
 
