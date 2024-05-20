b40eae41bc35329855dbd6c04365f196771ec38b
Nicolas Martinez <nicmart@gmail.com>
2024-05-20 13:48:59 -0700

    reset video player on video change despite same file played

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,8 +1,11 @@
 import "./VideoPlayer.scss";
 
 function VideoPlayer({ selectedVideo }) {
+  // Generate a unique key based on the selected video ID
+  const videoKey = selectedVideo ? selectedVideo.id : "no-video";
   return (
     <video
+      key={videoKey}
       className="video-player"
       src={selectedVideo.video}
       poster={selectedVideo.image}
